eb743d1593247c3c119439dcc0f7a270c31e558c
Mohammad Adil <mohammad.adil@ucertify.com>
2023-05-30 17:25:29 +0530

    30may

---
+++ src/ReverseOFNumber.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class ReverseOFNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        while (n>0){
+            int lastdigit = n%10;
+            System.out.print(lastdigit + " ");
+             n = n/10;
+        }
+        System.out.println();
+    }
+}
