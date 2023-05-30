848c152be91fea8ced779a3bfc639158563c0d27
Mohammad Adil <mohammad.adil@ucertify.com>
2023-05-30 16:55:48 +0530

    Java class

---
+++ src/AreaOfSqaure.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class AreaOfSqaure {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float side = sc.nextFloat();
+
+        float result = side *side;
+
+        System.out.println("Area of Circle " + result);
+    }
+}
+
---
+++ src/Averageof3.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Averageof3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        float A = sc.nextFloat();
+        float B = sc.nextFloat();
+        float C = sc.nextFloat();
+
+        float average = (A+B+C)/3;
+        System.out.println(average);
+    }
+}
